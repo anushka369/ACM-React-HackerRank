7ebccec2ec7dea4262f911ca78f70cd91526c0e5
anushka <81908360+anushka369@users.noreply.github.com>
2025-06-22 13:32:51 +0530

    App.test.js

--- Article Sorting/src/App.test.js
+++ Article Sorting/src/App.test.js
@@ -1,52 +1,106 @@
+import React from "react";
 import App from "./App";
+import { render, within, fireEvent, cleanup } from "@testing-library/react";
 
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { ARTICLES_DATA as articles } from "./constants";
 
-test("displays error message when fields are empty and submit is clicked", () => {
-  render(<App />);
-  const submitButton = screen.getByTestId("submit-button");
-  fireEvent.click(submitButton);
-  const errorMessage = screen.getByTestId("error-message");
-  expect(errorMessage.textContent).toBe("All fields are required.");
+const testIds = {
+  mostUpvotedLink: "most-upvoted-link",
+  mostRecentLink: "most-recent-link",
+  article: "article",
+};
+
+const mostUpvotedArticles = articles.concat().sort((a, b) => {
+  if (a.upvotes > b.upvotes) {
+    return -1;
+  }
+  if (a.upvotes < b.upvotes) {
+    return 1;
+  }
+  return 0;
 });
 
-test("displays submitted data when form is submitted with valid inputs", async () => {
-  render(<App />);
-  const nameInput = screen.getByTestId("name-input");
-  const emailInput = screen.getByTestId("email-input");
-  const messageInput = screen.getByTestId("message-input");
-  const submitButton = screen.getByTestId("submit-button");
+const mostRecentArticles = articles.concat().sort((a, b) => {
+  const aDate = new Date(a.date);
+  const bDate = new Date(b.date);
+  if (aDate > bDate) {
+    return -1;
+  }
+  if (aDate < bDate) {
+    return 1;
+  }
+  return 0;
+});
 
-  fireEvent.change(nameInput, { target: { value: "John Doe" } });
-  fireEvent.change(emailInput, { target: { value: "john@example.com" } });
-  fireEvent.change(messageInput, { target: { value: "Hello there!" } });
+const renderApp = () => render(<App articles={articles} />);
 
-  fireEvent.click(submitButton);
+beforeEach(() => {});
 
-  await waitFor(() => {
-    const submittedData = screen.getByTestId("submitted-data");
-    expect(submittedData).toBeInTheDocument();
-    expect(submittedData).toHaveTextContent("Name: John Doe");
-    expect(submittedData).toHaveTextContent("Email: john@example.com");
-    expect(submittedData).toHaveTextContent("Message: Hello there!");
+afterEach(() => {
+  cleanup();
+});
+
+const expectArticles = (articles, expectedArticles) => {
+  expect(articles.length).toBe(expectedArticles.length);
+  articles.forEach((article, i) => {
+    const title = within(article).getByTestId("article-title").textContent;
+    const upvotes = within(article).getByTestId("article-upvotes").textContent;
+    const date = within(article).getByTestId("article-date").textContent;
+    const expectedArticle = expectedArticles[i];
+    expect([title, upvotes, date]).toEqual([
+      expectedArticle.title,
+      expectedArticle.upvotes.toString(),
+      expectedArticle.date,
+    ]);
   });
+};
+
+test("Initial articles render correctly", () => {
+  const { queryAllByTestId } = renderApp();
+
+  const articles = queryAllByTestId(testIds.article);
+  expectArticles(articles, mostUpvotedArticles);
 });
 
-test("clears input fields after successful submission", () => {
-  render(<App />);
-  const nameInput = screen.getByTestId("name-input");
-  const emailInput = screen.getByTestId("email-input");
-  const messageInput = screen.getByTestId("message-input");
-  const submitButton = screen.getByTestId("submit-button");
+test("Clicking on top renders expected articles", () => {
+  const { getByTestId, queryAllByTestId } = renderApp();
 
-  fireEvent.change(nameInput, { target: { value: "John Doe" } });
-  fireEvent.change(emailInput, { target: { value: "john@example.com" } });
-  fireEvent.change(messageInput, { target: { value: "Hello there!" } });
-  
-  fireEvent.click(submitButton);
+  const mostUpvotedLink = getByTestId(testIds.mostUpvotedLink);
+  fireEvent.click(mostUpvotedLink);
 
-  expect(nameInput.value).toBe("");
-  expect(emailInput.value).toBe("");
-  expect(messageInput.value).toBe("");
+  const articles = queryAllByTestId(testIds.article);
+  expectArticles(articles, mostUpvotedArticles);
+});
+
+test("Clicking on newest renders expected articles", () => {
+  const { getByTestId, queryAllByTestId } = renderApp();
+
+  const mostRecentLink = getByTestId(testIds.mostRecentLink);
+  fireEvent.click(mostRecentLink);
+
+  const articles = queryAllByTestId(testIds.article);
+  expectArticles(articles, mostRecentArticles);
+});
+
+test("Sequence of navigation clicks renders expected articles", () => {
+  const { getByTestId, queryAllByTestId } = renderApp();
+
+  const mostUpvotedLink = getByTestId(testIds.mostUpvotedLink);
+  const mostRecentLink = getByTestId(testIds.mostRecentLink);
+
+  const elements = [
+    mostRecentLink,
+    mostUpvotedLink,
+    mostUpvotedLink,
+    mostRecentLink,
+    mostRecentLink,
+    mostUpvotedLink,
+  ];
+  for (const elem of elements) {
+    fireEvent.click(elem);
+    const articles = queryAllByTestId(testIds.article);
+    const expectedArticles =
+      elem === mostUpvotedLink ? mostUpvotedArticles : mostRecentArticles;
+    expectArticles(articles, expectedArticles);
+  }
 });
